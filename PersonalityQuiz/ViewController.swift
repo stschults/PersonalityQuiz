c16cd30ffa172ad053625b5bd0442245ac954191
Stanislav Schults <stschults@yandex.ru>
2022-06-09 19:11:59 +0300

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Станислав Шульц on 09.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
